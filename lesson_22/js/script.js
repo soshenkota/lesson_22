301a3a3fb0b25ef2f5c4fa0dea8db1d806e5fd8b
soshenkota <114571243+soshenkota@users.noreply.github.com>
2023-03-04 20:32:18 +0200

    123

---
+++ lesson_22/js/script.js
@@ -0,0 +1,13 @@
+"use strict"
+console.log('Я вчу JS!');
+
+let user;
+let userName = 'Вася';
+user = userName;
+console.log(user);
+let user2 = 145 / 0;
+console.log(user2);
+let userName2;
+console.log(typeof userName2);
+let userNumber = "45" / "8";
+console.log(typeof userNumber);
